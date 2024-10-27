8a2fb367b1f2e3c9eb78f6f0d38bb1a3d5b8517e
sushant-manjaro-pc <111882164+sushant7326@users.noreply.github.com>
2024-10-27 13:56:56 +0530

    connected database

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const Transaction = require('./api/models/Transaction.js');
+const mongoose = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +13,11 @@ app.get('/api/test', (req, res) => {
     res.json('test ok');
 });
 
-app.post('/api/transaction', (req, res) => {
-    res.json(req.body);
+app.post('/api/transaction', async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL);
+    const {name, description, datetime} = req.body;
+    const transaction = await Transaction.create({name, description, datetime});
+    res.json(transaction);
 });
 
 app.listen(process.env.PORT);
