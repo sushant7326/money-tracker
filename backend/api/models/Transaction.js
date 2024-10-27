078e2fc3e571a69c09e868113aa99aa16687d78c
sushant-manjaro-pc <111882164+sushant7326@users.noreply.github.com>
2024-10-27 13:58:50 +0530

    added price to mongoose model

--- backend/api/models/Transaction.js
+++ backend/api/models/Transaction.js
@@ -4,6 +4,7 @@ const { model, Schema } = mongoose;
 
 const TransactionSchema = new Schema({
     name:{type:String, required:true},
+    price:{type:Number, required:true},
     description:{type:String, required:true},
     datetime:{type:Date, required:true},
 });
